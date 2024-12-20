979f446580aa2299f47e83886520c59036c32cb1
carlosuc3m <100329787@alumnos.uc3m.es>
2024-12-20 13:09:09 +0100

    add more information to errors

--- src/main/java/io/bioimage/modelrunner/tensorflow/v1/Tensorflow1Interface.java
+++ src/main/java/io/bioimage/modelrunner/tensorflow/v1/Tensorflow1Interface.java
@@ -220,11 +220,11 @@ public class Tensorflow1Interface implements DeepLearningEngineInterface {
 		if (task.status == TaskStatus.CANCELED)
 			throw new RuntimeException();
 		else if (task.status == TaskStatus.FAILED)
-			throw new RuntimeException();
+			throw new RuntimeException(task.error);
 		else if (task.status == TaskStatus.CRASHED) {
 			this.runner.close();
 			runner = null;
-			throw new RuntimeException();
+			throw new RuntimeException(task.error);
 		}
 	}
 	
@@ -380,11 +380,11 @@ public class Tensorflow1Interface implements DeepLearningEngineInterface {
 			if (task.status == TaskStatus.CANCELED)
 				throw new RuntimeException();
 			else if (task.status == TaskStatus.FAILED)
-				throw new RuntimeException();
+				throw new RuntimeException(task.error);
 			else if (task.status == TaskStatus.CRASHED) {
 				this.runner.close();
 				runner = null;
-				throw new RuntimeException();
+				throw new RuntimeException(task.error);
 			}
 			for (int i = 0; i < outputTensors.size(); i ++) {
 	        	String name = (String) Types.decode(encOuts.get(i)).get(MEM_NAME_KEY);
@@ -508,11 +508,11 @@ public class Tensorflow1Interface implements DeepLearningEngineInterface {
 			if (task.status == TaskStatus.CANCELED)
 				throw new RuntimeException();
 			else if (task.status == TaskStatus.FAILED)
-				throw new RuntimeException();
+				throw new RuntimeException(task.error);
 			else if (task.status == TaskStatus.CRASHED) {
 				this.runner.close();
 				runner = null;
-				throw new RuntimeException();
+				throw new RuntimeException(task.error);
 			}
 			this.runner.close();
 			this.runner = null;
